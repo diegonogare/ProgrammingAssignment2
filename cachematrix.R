ce5661531e998bdc38d6944e5299c9a47c8f98b2
DiegoNogare <nogare@outlook.com>
2014-07-27 12:30:00 -0300

    Exercicio resolvido

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## This function is responsible to receive a matrix and set this value to Mat.
+## next steps is define methods GET and SET, called GetSolve and SetSolve, used to 
+## store result cached.
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- Mat
+    m <<- NULL
+  }
+  get <- function() x
+  SetSolve <- function(solve) m <<- solve
+  GetSolve <- function() m
+  list(set = set, get = get, SetSolve = SetSolve, GetSolve = GetSolve)
 }
 
-
-## Write a short comment describing this function
-
+## This second function inverse the matrix
+## checking before processig if wasn't already inversed
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$GetSolve()
+  ## this code block check the inversed.
+  if(!is.null(m)) {
+    message("getting cached data [Recuperando dados do cache]") ## The message should be displayed only if the matrix alread solved.
+    return(m)
+  }
+  
+  data <- x$get()
+  m <- solve(data, ...)
+  x$SetSolve(m)
+  m
 }
